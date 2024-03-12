40344dd80b83b87c96493efd6b6fb10c32553945
sudeepmi <sudeepm0599@gmail.com>
2024-03-12 10:10:35 +0545

    fix: url update

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://anyscrap.herokuapp.com/api'
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5000/api' : 'https://anyscrap.onrender.com/api'
 export default  axios.create({
     baseURL: baseURL,
         headers: {
